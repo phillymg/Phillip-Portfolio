118bd3b416bf32f19057a92247966975cf921ece
Philmg <phillipmguetterman@att.net>
2023-06-24 16:19:32 -0500

    Add Javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+function scrollFunction1() {
+    let e = document.getElementById("contact");
+    e.scrollIntoView({
+        block:'start',
+        behavior: 'smooth',
+        inline:'start'
+    })
+}
+
+function scrollFunction2() {
+    let e = document.getElementById("my-work");
+    e.scrollIntoView({
+        block:'start',
+        behavior: 'smooth',
+        inline:'start'
+    })
+}
+
+function scrollFunction3() {
+    let e = document.getElementById("about-me");
+    e.scrollIntoView({
+        block:'start',
+        behavior: 'smooth',
+        inline:'start'
+    })
+}
